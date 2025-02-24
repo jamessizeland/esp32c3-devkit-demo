ceffd28cccce60b0f837cb2881af9a74c6417f1e
James Sizeland <jamessizeland@gmail.com>
2025-02-24 22:58:06 +0000

    comment out unused mods

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,11 @@
 use ector::mutex::NoopRawMutex;
 use embassy_sync::channel::Sender;
 
-pub mod ambient;
-pub mod ble;
+// pub mod ambient;
+// pub mod ble;
 pub mod bsp;
-pub mod buttons;
-pub mod imu;
+// pub mod buttons;
+// pub mod imu;
 pub mod led;
 
 /// Alias for the actor's inbox
