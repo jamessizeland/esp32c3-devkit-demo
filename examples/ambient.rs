1a4af34881056e974db90f76c6092dd1418dc4a3
James Sizeland <jamessizeland@gmail.com>
2025-02-25 22:50:53 +0000

    add documentation

--- examples/ambient.rs
+++ examples/ambient.rs
@@ -1,3 +1,11 @@
+//! # Ambient Sensor Example
+//!
+//! This example demonstrates how to read the temperature and humidity from the onboard SHTC3 sensor.
+//! The SHTC3 sensor is a low-power sensor that can be used to measure temperature and humidity.
+//! The sensor has a slow read time, so it is recommended to read the sensor no more than every 20 seconds.
+//! This example demonstrates how to use an actor to read the sensor every 20 seconds.
+//! The actor will read the sensor for 1 minute before stopping.
+
 #![no_std]
 #![no_main]
 
@@ -8,7 +16,10 @@ use esp_backtrace as _;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 
-use esp32c3_devkit_demo::{ambient, bsp::Board};
+use esp32c3_devkit_demo::{
+    ambient::{self, Message},
+    bsp::Board,
+};
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) -> ! {
@@ -16,24 +27,27 @@ async fn main(spawner: Spawner) -> ! {
 
     let board = Board::init();
 
-    let actor = ambient::spawn_actor(
-        spawner,
-        ambient::Config {
-            i2c_bus: board.i2c_bus,
-        },
-    )
-    .expect("failed to spawn ambient actor");
+    // For now we only have one element in the configuration.
+    // But we could add more elements to the configuration to pass to the actor.
+    let config = ambient::Config {
+        i2c_bus: board.i2c_bus,
+    };
+    let actor = ambient::spawn_actor(spawner, config).expect("failed to spawn");
+
+    // Set the power mode to normal mode.
     actor
-        .send(ambient::Message::SetPowerMode(shtcx::PowerMode::NormalMode))
+        .send(Message::SetPowerMode(shtcx::PowerMode::NormalMode))
         .await;
+
     Timer::after_secs(1).await;
 
-    actor
-        .send(ambient::Message::Start(Duration::from_secs(20)))
-        .await;
+    // Start the actor to read the temperature and humidity every 20 seconds.
+    // This device has a slow read time.
+    actor.send(Message::Start(Duration::from_secs(20))).await;
 
+    // Stop the actor after 60 seconds.
     Timer::after_secs(60).await;
-    actor.send(ambient::Message::Stop).await;
+    actor.send(Message::Stop).await;
 
     pending().await
 }
--- examples/led.rs
+++ examples/led.rs
@@ -1,3 +1,9 @@
+//! # LED Example
+//!
+//! This example demonstrates how to control the onboard LED using an actor.
+//! The actor will control the LED asynchronously.
+//! The actor will run a sequence of colours forever.
+
 #![no_std]
 #![no_main]
 
@@ -9,7 +15,10 @@ use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use smart_leds::colors::{BLUE, GREEN, RED};
 
-use esp32c3_devkit_demo::{bsp::Board, led};
+use esp32c3_devkit_demo::{
+    bsp::Board,
+    led::{self, Message, Repeat},
+};
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) -> ! {
@@ -25,17 +34,18 @@ async fn main(spawner: Spawner) -> ! {
     // The actor inbox can be shared with other actors to send messages to this actor.
     let led_actor = led::spawn_actor(spawner, led::Config { led: board.led })
         .expect("failed to spawn led actor");
-    led_actor.send(led::Message::SetBrightness(50)).await;
-    led_actor.send(led::Message::SetColour(RED)).await;
+    led_actor.send(Message::SetBrightness(50)).await;
+    led_actor.send(Message::SetColour(RED)).await;
     Timer::after_secs(1).await;
 
     // This sequence will run forever until the actor is dropped, or another message is sent.
+    // It will run as a background task.
     let sequence = &[RED, GREEN, BLUE];
     led_actor
-        .send(led::Message::SetSequence((
+        .send(Message::SetSequence((
             sequence,
             Duration::from_secs(1),
-            led::Repeat::Forever,
+            Repeat::Forever,
         )))
         .await;
 
